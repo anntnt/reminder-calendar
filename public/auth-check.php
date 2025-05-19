b420a153481899424e3689649aa306d8d1aafb26
Ann Tran <anpjure@gmail.com>
2025-05-19 20:42:06 +0200

    Remove log messages

--- public/auth-check.php
+++ public/auth-check.php
@@ -6,9 +6,9 @@ require '../config/bootstrap.php';
 require '../app/auth.php';
 
 //debug
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 
 
 header('Content-Type: application/json');
